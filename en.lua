7a02725919c18512b16c957a0ff7f7730ff1af2d
Matti <57048210+MattiVboiii@users.noreply.github.com>
2024-09-02 17:02:27 +0200

    Create en.lua

---
+++ en.lua
@@ -0,0 +1,32 @@
+command = {
+        plyrnotfound = 'Player not found.',
+        invalid_id = 'No valid ID',
+        own_id = 'You cannot use your own ID for this',
+        arg_help = 'Player ID.
+        duty = { 
+            help = 'Go on/off duty',
+            on = 'You are now on duty',
+            off = 'You are now off duty.',
+        },
+        tpmzb = {
+            help = 'Teleport to the maze bank tower (Admin only)',
+            success = 'Teleported to the maze bank tower.'
+        },
+        tpmc = {
+            help = 'Teleport to mount chiliad (Admin only)',
+            success = 'Teleported to mount chiliad.'
+        },
+        tptraining = {
+            help = 'Teleport to police training (Admin only)',
+            success = 'Teleported to police training.'
+        },
+        bring = {
+            help = 'Brings a player to you (Admin only)',
+            success = 'Player has been brought to your location.'
+        },
+        defaultinv = {
+            help = 'Gives the player default DEV items (Admin only)',
+            success = 'You have been given your default DEV items.'
+        },
+
+    }
