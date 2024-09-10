b91da8b9880636970fc9e654dac5849ce580d791
Matti <57048210+MattiVboiii@users.noreply.github.com>
2024-09-10 13:17:57 +0200

    Update en.lua for /cid

--- en.lua
+++ en.lua
@@ -9,19 +9,19 @@ command = {
             off = 'You are now off duty.',
         },
         tpmzb = {
-            help = 'Teleport to the maze bank tower (Admin only)',
+            help = 'Teleport to the maze bank tower (Admin Only)',
             success = 'Teleported to the maze bank tower.'
         },
         tpmc = {
-            help = 'Teleport to mount chiliad (Admin only)',
+            help = 'Teleport to mount chiliad (Admin Only)',
             success = 'Teleported to mount chiliad.'
         },
         tptraining = {
-            help = 'Teleport to police training (Admin only)',
+            help = 'Teleport to police training (Admin Only)',
             success = 'Teleported to police training.'
         },
         bring = {
-            help = 'Brings a player to you (Admin only)',
+            help = 'Brings a player to you (Admin Only)',
             success = 'Player has been brought to your location.'
         },
         reviveall = {
@@ -29,8 +29,11 @@ command = {
             success = 'All players are revived.',
         },
         defaultinv = {
-            help = 'Gives the player default DEV items (Admin only)',
-            success = 'You have been given your default DEV items.'
+            help = 'Gives the player the default items (Admin Only)',
+            success = 'You have been given your default items.'
         },
-
+        cid = {
+            help = 'Shows the citizenid of a specified player (Admin Only)',
+            success = 'The citizenid of the player is: %{citizenid}.',
+        },
     }
